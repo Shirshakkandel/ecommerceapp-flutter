f47543e2464f7084c6dd82fdba9be8eedb46ac45
SHIRSHAK\shirshak <shirshakkandel@gmail.com>
2020-07-29 09:11:52 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+runApp(
+  new MaterialApp(
+    home:new Center( //Material object(app) have property home with center widget
+      child: new Text('Hello world'),
+    )
+  )
+);
+}
