5406c9008bfe2ed84b51cdb116067c427072e1dd
SHIRSHAK\shirshak <shirshakkandel@gmail.com>
2020-08-01 16:55:18 +0545

    Categories(horizontal) and product is added

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
-  runApp
-  (
-    MaterialApp(
-      //debugShowCheckedModeBanner: false,
-      home:HomePage()
-    )
-  );
-}
+import 'package:carousel_pro/carousel_pro.dart';
+//my own import
+import 'components/horizontal_listview.dart';
+import 'components/product.dart';
 
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
+}
 
 class HomePage extends StatefulWidget {
   @override
@@ -20,106 +17,137 @@ class HomePage extends StatefulWidget {
 class _State extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold
-    (
-        appBar: new AppBar(
+    Widget image_carousel = new Container(
+      height: 200.0,
+      child: new Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          AssetImage('images/c1.jpg'),
+          AssetImage('images/m1.jpeg'),
+          AssetImage('images/w3.jpeg'),
+          AssetImage('images/w4.jpeg'),
+          AssetImage('images/m2.jpg'),
+        ],
+        autoplay: false,
+        animationCurve: Curves.fastOutSlowIn,
+        animationDuration: Duration(milliseconds: 1000),
+        dotSize: 4.0,
+        indicatorBgPadding: 2.0,
+      ),
+    );
+
+    return Scaffold(
+      appBar: new AppBar(
+        elevation: 0.1,
         backgroundColor: Colors.red,
         title: Text('Sabfeed'),
-        actions: <Widget>
-        [
-          new IconButton(icon: Icon(Icons.search,color:Colors.white), onPressed: (){}),
-          new IconButton(icon: Icon(Icons.shopping_cart,color:Colors.white), onPressed: (){})
+        actions: <Widget>[
+          new IconButton(
+              icon: Icon(Icons.search, color: Colors.white), onPressed: () {}),
+          new IconButton(
+              icon: Icon(Icons.shopping_cart, color: Colors.white),
+              onPressed: () {})
         ],
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            //header
+            new UserAccountsDrawerHeader(
+              accountName: Text('Shirshak kandel'),
+              accountEmail: Text('shirshakkandel@gmail.com'),
+              currentAccountPicture: GestureDetector(
+                child: new CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(Icons.person, color: Colors.white),
+                ),
+              ),
+              decoration: new BoxDecoration(color: Colors.red),
+            ),
+
+            //body
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Home Page'),
+                leading: Icon(Icons.home),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('My account'),
+                leading: Icon(Icons.person),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('My orders'),
+                leading: Icon(Icons.shopping_basket),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Categories'),
+                leading: Icon(Icons.dashboard),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Favourite'),
+                leading: Icon(Icons.favorite),
+              ),
+            ),
+            Divider(),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Setting'),
+                leading: Icon(Icons.settings, color: Colors.blue),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('About'),
+                leading: Icon(Icons.help, color: Colors.green),
+              ),
+            ),
+          ],
         ),
-      
-
-
-      drawer: new Drawer(child: new ListView(children: <Widget>
-      [
-          //header 
-          new UserAccountsDrawerHeader(accountName: Text('Shirshak kandel'), accountEmail: Text('shirshakkandel@gmail.com'),
-          currentAccountPicture: GestureDetector(child:new CircleAvatar(backgroundColor: Colors.grey,
-          child: Icon(Icons.person,color: Colors.white),
-       
+      ),
+      body: new ListView(
+        children: <Widget>[
+          //image carousel begin
+          image_carousel,
+          //padding widget
+          new Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: new Text('Categories'),
           ),
-           
+          HorizontalList(),
+
+          //padding widget
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text('Product'),
           ),
-          decoration: new BoxDecoration(
-            color:Colors.red
+          Container(
+            height: 320.0,
+            child: Product(),
           ),
-          ),
-
-          //body
-
-              InkWell(
-                  onTap:() {},
-                   child: ListTile(
-                  title: Text('Home Page'),
-                  leading: Icon(Icons.home),
-
-                ),
-              ),
-
-                InkWell(
-                  onTap:() {},
-                   child: ListTile(
-                  title: Text('My account'),
-                  leading: Icon(Icons.person),
-
-                ),
-              ),
-
-                InkWell(
-                  onTap:() {},
-                   child: ListTile(
-                  title: Text('My orders'),
-                  leading: Icon(Icons.shopping_basket),
-
-                ),
-              ),
-
-                InkWell(
-                  onTap:() {},
-                   child: ListTile(
-                  title: Text('Categories'),
-                  leading: Icon(Icons.dashboard),
-
-                ),
-              ),
-
-                InkWell(
-                  onTap:() {},
-                   child: ListTile(
-                  title: Text('Favourite'),
-                  leading: Icon(Icons.favorite),
-
-                ),
-              ),
-              Divider(),
-
-
-                InkWell(
-                  onTap:() {},
-                   child: ListTile(
-                  title: Text('Setting'),
-                  leading: Icon(Icons.settings),
-
-                ),
-              ),
-
-                 InkWell(
-                  onTap:() {},
-                   child: ListTile(
-                  title: Text('About'),
-                  leading: Icon(Icons.help),
-
-                ),
-              ),
-       ],
-          ),),
-
-        );
-    }
-
+        ],
+      ),
+    );
+  }
 }
-
